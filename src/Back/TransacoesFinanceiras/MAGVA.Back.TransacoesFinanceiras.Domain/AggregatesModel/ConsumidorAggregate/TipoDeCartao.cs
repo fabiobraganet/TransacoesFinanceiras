8f5bf5c73faf45860c04bb69cd3119b1a8028baa
Fabio Braga <fabiobraga.net@gmail.com>
2019-05-27 19:02:58 -0300

    Alterando o nome da entidade de dominio

---
+++ src/Back/TransacoesFinanceiras/MAGVA.Back.TransacoesFinanceiras.Domain/AggregatesModel/ConsumidorAggregate/TipoDeCartao.cs
@@ -0,0 +1,17 @@
+﻿
+namespace MAGVA.Back.TransacoesFinanceiras.Domain.AggregatesModel.ConsumidorAggregate
+{
+    using Seedwork;
+
+    public class TipoDeCartao
+        : Enumeration
+    {
+        public static TipoDeCartao Chip = new TipoDeCartao(1, "Chip");
+        public static TipoDeCartao TarjaMagnetica = new TipoDeCartao(2, "Tarja Magnética");
+
+        public TipoDeCartao(int id, string name)
+            : base(id, name)
+        {
+        }
+    }
+}
