94b92d21652bab1de40ee9e68ae53ad318a9b234
Fabio Braga <fabiobraga.net@gmail.com>
2019-05-28 21:44:26 -0300

    inclusão do comando criar novo consumidor

--- src/Back/TransacoesFinanceiras/MAGVA.Back.TransacoesFinanceiras/Application/Command/CriarConsumidorCommand.cs
+++ src/Back/TransacoesFinanceiras/MAGVA.Back.TransacoesFinanceiras/Application/Command/CriarConsumidorCommand.cs
@@ -1,11 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
+﻿
 namespace MAGVA.Back.TransacoesFinanceiras.Application.Command
 {
+    using MediatR;
+    using System;
+    using System.Runtime.Serialization;
+    
+
+    [DataContract]
     public class CriarConsumidorCommand
+         : IRequest<bool>
     {
+
+        [DataMember]
+        public string Nome { get; private set; }
+        [DataMember]
+        public string Email { get; set; }
+        [DataMember]
+        public bool Ativo { get; set; }
+
+        public CriarConsumidorCommand()
+        {
+        }
+
+        public CriarConsumidorCommand(
+            string nome,
+            string email,
+            bool ativo = true)
+        {
+            Nome = nome;
+            Email = email;
+            Ativo = ativo;
+        }
     }
 }
