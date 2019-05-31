68b9c40cd36cca0c8f91b04e3232f0dd4b50736e
Fabio Braga <fabiobraga.net@gmail.com>
2019-05-31 00:42:54 -0300

    Incluido o projeto de testes

---
+++ src/Back/TransacoesFinanceiras/MAGVA.Back.TransacoesFinanceiras.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace MAGVA.Back.TransacoesFinanceiras.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
--- src/Back/WatchDog/MAGVA.Back.WatchDog/Program.cs
+++ src/Back/WatchDog/MAGVA.Back.WatchDog/Program.cs
@@ -3,9 +3,6 @@ namespace MAGVA.Back.WatchDog
 {
     using Microsoft.AspNetCore;
     using Microsoft.AspNetCore.Hosting;
-    //using Microsoft.AspNetCore.Server.Kestrel.Core;
-    //using System;
-    //using System.Net;
 
     public class Program
     {
@@ -17,16 +14,5 @@ namespace MAGVA.Back.WatchDog
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
-                //.UseKestrel()
-                //.ConfigureKestrel((context, options) =>
-                //        {
-                //            options.Limits.MaxConcurrentConnections = 100;
-                //            options.Limits.MaxConcurrentUpgradedConnections = 100;
-                //            options.Limits.MaxRequestBodySize = 10 * 1024;
-                //            options.Limits.MinRequestBodyDataRate = new MinDataRate(bytesPerSecond: 100, gracePeriod: TimeSpan.FromSeconds(10));
-                //            options.Limits.MinResponseDataRate = new MinDataRate(bytesPerSecond: 100, gracePeriod: TimeSpan.FromSeconds(10));
-                //            options.Listen(IPAddress.Loopback, 15098);
-                //            options.Listen(IPAddress.Loopback, 15099, listenOptions => {});
-                //        });
     }
 }
